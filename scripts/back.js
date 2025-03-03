ed615d3a5efca08fb6c77744710890084014ed54
altr-r <arafatrahman0012@gmail.com>
2025-03-04 01:09:56 +0600

    fixed fixed

--- scripts/back.js
+++ scripts/back.js
@@ -1,5 +1,5 @@
 const back = document.getElementById("back");
 
 back.addEventListener("click", () => {
-  window.location.href = "/A5-Dev-Board/index.html";
+  window.location.href = "/A5-Dev-Board/";
 });
