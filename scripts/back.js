b4c672f9cb25f262b085a5e1b447c21896ed5984
altr-r <arafatrahman0012@gmail.com>
2025-03-04 01:13:51 +0600

    final-final-final fix

--- scripts/back.js
+++ scripts/back.js
@@ -1,5 +1,5 @@
 const back = document.getElementById("back");
 
 back.addEventListener("click", () => {
-  window.location.href = window.location.origin + "/index.html";
+  window.location.href = "./index.html";
 });
